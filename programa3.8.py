5c9c043c2c836c64ecc195b4e0f172b1baf73963
github.com/nicolyy <nicollyoliveira003@gmail.com>
2023-04-22 22:20:45 -0300

    Exercicio Resolvido

--- programa3.8.py
+++ programa3.8.py
@@ -1,2 +1,6 @@
 #Escreva um porgrama que leia um valor em metros
 #e o exiba convertido em milimentros
+
+numero = float(input("DIgite o número em metros: "))
+conversao = numero * 1000
+print(f"{numero} em metros, convertido em milimetros é {conversao}")
