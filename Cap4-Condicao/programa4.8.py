f4a5b30504546dfb1cbcf6b41977f71221095a67
github.com/nicolyy <nicollyoliveira003@gmail.com>
2023-08-09 22:33:38 -0300

    Exercicio feito

--- Cap4-Condicao/programa4.8.py
+++ Cap4-Condicao/programa4.8.py
@@ -1,3 +1,18 @@
 #escreva um programa que leia dois numeros e que pergunte qual operação você deseja realizar
 #Você deve poder caucular soma, subtração, multiplicação ou divisão. Exiba o resultado 
 #da operação solicitada
+
+n1 = int(input("Digite o primeiro numero: "))
+n2 = int(input("Digite o segundo numero: "))
+print("Selecione o operador que deseja")
+print(" Soma = 1\n Subtração = 2\n Multiplicação = 3\n Divisão = 4")
+operador = int(input("Qual o operador?: "))
+if operador == 1:
+    operacao = n1 + n2
+elif operador == 2:
+    operacao = n1 - n2
+elif operador == 3:
+    operacao = n1 * n2
+elif operador == 4:
+    operacao = n1 / n2
+print(f"Resultado = {operacao}")
