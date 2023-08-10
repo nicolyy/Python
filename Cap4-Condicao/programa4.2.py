519ebd76edf9cda9117ebd9eea370fcfa7fa465f
github.com/nicolyy <nicollyoliveira003@gmail.com>
2023-08-09 22:33:52 -0300

    Exercicio feito

--- Cap4-Condicao/programa4.2.py
+++ Cap4-Condicao/programa4.2.py
@@ -1,3 +1,13 @@
 #Escreva um programa que pergunte a velocidade do carro de um usuário. 
 #Caso ultrapasse 80km/h, exiba uma mensagem dizendo que o usuário foi multado
 #Nesse caso, exiba o valor da multa, cobrando R$: 5 por km acima de 80 km/h
+
+velocidade = int(input("Qual a velocidade do carro: "))
+if velocidade > 80:
+    multado = True
+    print("Você foi multado")
+    if True:
+        multa = (velocidade - 80) * 5
+        print(f"Sua multa é {multa}")
+
+    
