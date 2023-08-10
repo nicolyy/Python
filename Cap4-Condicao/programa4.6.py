a383097fb63deb4a925922b341aa4dfe818541fc
github.com/nicolyy <nicollyoliveira003@gmail.com>
2023-08-09 22:33:42 -0300

    Exercicio feito

--- Cap4-Condicao/programa4.6.py
+++ Cap4-Condicao/programa4.6.py
@@ -1,3 +1,10 @@
 #Escreva um programa que pergunte a distância que um passageirodeseja percorrer em km. 
 #Caucule o preço da passagem, cobrando R$ 0,50 por km para viagens de até 200km e 
 #R$ 0,45 para viagens mais longas
+
+distancia = int(input("Qual a distancia em KM?: "))
+if distancia <= 200:
+    cobrar = 0.50
+elif distancia >= 200:
+    cobrar = 0.45
+print(f"O valor total da sua vaigem é {distancia * cobrar}")
