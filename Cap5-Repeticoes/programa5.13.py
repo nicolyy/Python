82663d53831a3b1c95137e058c8875688fde32b0
github.com/nicolyy <nicollyoliveira003@gmail.com>
2023-08-10 21:24:54 -0300

    Exercicio Novo

---
+++ Cap5-Repeticoes/programa5.13.py
@@ -0,0 +1,4 @@
+#Escreva um programa  que pergunte o valor inicial de uma divida e o juros mensal.
+# Pergunte também o valor mensal que será pago.
+# Imprima o número de meses para que a divida seja paga, 
+# o total pago e o total de juros pago.
---
+++ Cap5-Repeticoes/programa5.15.py
@@ -0,0 +1,5 @@
+#Escreva um programa para controlar uma pequena máquina registradora.
+# Você deve solicitar ao usuário que digite o código do produto e a quantidade comprada.
+# Utilize a tabela de códigos abaixo para obter o preço de cada produto:
+#Seu programa deve exibir o total das compras depois que o usuário digitar 0 (zero).
+#Qualquer outro codigo deve gerar a mensagem de erro "Codigo invalido".
