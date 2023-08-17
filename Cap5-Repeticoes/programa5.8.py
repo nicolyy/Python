334ed90517b19e6d838f2f152d9cdf6decbfd69d
github.com/nicolyy <nicollyoliveira003@gmail.com>
2023-08-16 22:41:46 -0300

    Exercicio feito

--- Cap5-Repeticoes/programa5.8.py
+++ Cap5-Repeticoes/programa5.8.py
@@ -2,3 +2,13 @@
 #pelo segundo. Utilize apenas os operadores de soma e subtração para calcular o resultado.
 #Lembre-se de que podemos entender a multiplicação de dois numeros como somas sucessivas de um deles.
 #Assim, 4 x 5 = 5 + 5 + 5 + 5 = 4 + 4 + 4 + 4 + 4.
+
+numero1 = int(input("Digite o primeiro número: "))
+numero2 = int(input("Digite o segundo número: "))
+
+resultado = 0
+while numero2 > 0:
+    resultado = resultado + numero1
+    numero2 = numero2 - 1
+
+print(f"O resultado da multiplicação é: {resultado}")
