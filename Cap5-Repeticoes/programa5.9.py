708d87f882c7063970cf47af228c0c7f5645541d
github.com/nicolyy <nicollyoliveira003@gmail.com>
2023-08-16 22:41:49 -0300

    Exercicio feito

--- Cap5-Repeticoes/programa5.9.py
+++ Cap5-Repeticoes/programa5.9.py
@@ -1,4 +1,12 @@
-#Escreva um programa que leia dois numeros. Imprima a divisão inteira do primeiro pelo segundo,
-#assim como o resto da divisão. Utilize apenas os operadores de soma e subtração para calcular o resultado.
-#Lembre-se de que podemos entender o quociente da divisão de dois numeros como a quantidade de vezes que podemos retirar o divisor do dividendo.
-#Logo, 20 / 4 = 5, uma vez que podemos subtrair 4 cinco vezes de 20.
+dividendo = int(input("Digite o dividendo: "))
+divisor = int(input("Digite o divisor: "))
+
+quociente = 0
+while dividendo >= divisor:
+    dividendo -= divisor
+    quociente += 1
+
+resto = dividendo
+
+print(f"Quociente: {quociente}")
+print(f"Resto: {resto}")
