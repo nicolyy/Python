80ee09f04fde54b8f0da1de11bc40b55c878bae5
github.com/nicolyy <nicollyoliveira003@gmail.com>
2023-08-16 22:41:31 -0300

    Exercicio feito

--- Cap5-Repeticoes/programa5.4.py
+++ Cap5-Repeticoes/programa5.4.py
@@ -1,2 +1,11 @@
 #Modifique o programa anterior para imprimir de 1 até o numero digitado 
 #pelo usuario, mas dessa vez, apenas os numeros impares
+
+fim = int(input("Digite o ultimo numero a imprimir: "))
+x = 1
+
+while x <= fim:
+    if x % 3 == 0:
+        print(x)
+    x = x + 1
+print("Muito hacker")
