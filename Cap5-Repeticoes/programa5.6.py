93633043502f70b84e3b658392066f4e131a1023
github.com/nicolyy <nicollyoliveira003@gmail.com>
2023-08-16 22:41:39 -0300

    Exercicio feito

--- Cap5-Repeticoes/programa5.6.py
+++ Cap5-Repeticoes/programa5.6.py
@@ -1,1 +1,9 @@
-#Altere o programa anterior para exibir os resultados no mesmo formato de uma tabuada: 2x1 = 2, 2x2=4, ...
+#Altere o programa anterior para exibir os resultados
+# no mesmo formato de uma tabuada: 2x1 = 2, 2x2=4, ...
+
+n = int(input("Digite qual tabuada você quer(1, 2, 3..): "))
+x = 1
+
+while x <= 10:
+    print(f"[{n} x {x} = {n*x}]")
+    x = x + 1
