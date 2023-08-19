08aec5ad9c292e5280ffc9d0bac6e235756bbe7d
github.com/nicolyy <nicollyoliveira003@gmail.com>
2023-08-18 23:33:19 -0300

    Exercicio feito

--- Cap5-Repeticoes/programa5.10.py
+++ Cap5-Repeticoes/programa5.10.py
@@ -1,1 +1,16 @@
-#Modifique o programa anterior para que aceite respostas com letras maiusculas e minusculas em todas as questoes.
+#Modifique o programa anterior para que aceite respostas com 
+# letras maiusculas e minusculas em todas as questoes.
+
+pontos = 0
+questao = 1
+
+while questao <= 3:
+    resposta = input(str(f"Resposta da questao {questao}: "))
+    if questao == 1 and resposta == "b" or resposta == "B":
+        pontos = pontos + 1
+    if questao == 2 and resposta == "a" or resposta == "A":
+        pontos = pontos + 1
+    if questao == 3 and resposta == "d" or resposta == "D":
+        pontos = pontos + 1
+    questao = questao + 1 
+print(f"O aluno fez {pontos} pontos")
