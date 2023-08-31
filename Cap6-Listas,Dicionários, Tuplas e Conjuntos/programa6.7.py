939e318f9c0efe861a23a4829cbae38c8a1db029
github.com/nicolyy <nicollyoliveira003@gmail.com>
2023-08-30 22:11:31 -0300

    Exercicio Novo

---
+++ Cap6-Listas,Dicionários, Tuplas e Conjuntos/programa6.7.py
@@ -0,0 +1,5 @@
+#Faça um programa que leia uma expressão com parenteses. Usando pilhas, verfique se os parenteses 
+# foram abertos e fechados na ordem correta.
+#Você pode adicionar elementos a pilha sempre que encontrar abre parenteses e desempilhá-la a cada
+#fecha parentese . Ao desempilhar, verifique se o topo da pilha é um abre parentese.
+# Se a expressão estiver correta, sua pilha estará vazia ao final.
