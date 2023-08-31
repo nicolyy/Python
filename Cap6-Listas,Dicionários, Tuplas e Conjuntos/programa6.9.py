9b60997c39eeb509c72ce0e51361f3a9939f48cc
github.com/nicolyy <nicollyoliveira003@gmail.com>
2023-08-30 22:11:38 -0300

    Exercicio Novo

---
+++ Cap6-Listas,Dicionários, Tuplas e Conjuntos/programa6.9.py
@@ -0,0 +1,2 @@
+#Modfique o exemplo para pesquisar dois valores. Em vez de apenas p, leia outro valor v 
+# que tambem sera procurado. Na impressao, indique qual dos dois valores foi achado primeiro.
